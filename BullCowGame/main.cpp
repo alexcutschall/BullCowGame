0d6015adc9d11909dc5727d8687c0f3511a8a49a
alexcutschall <alexander.cutschall@gmail.com>
2018-03-28 11:59:36 -0600

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Alexander Joseph Cutschall on 3/28/18.
+//  Copyright © 2018 Alexander Joseph Cutschall. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
